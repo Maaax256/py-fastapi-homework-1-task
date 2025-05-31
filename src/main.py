e2e429f894dd5bbd56b13cfa7425cd31599e1a26
Maksym Omelkov <omelkov85@gmail.com>
2025-06-01 02:35:39 +0300

    Movie Theater API Project (1.0)

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
 
-from database import init_db, close_db
-from routes import movie_router
+from src.database import init_db, close_db
+from src.routes import movie_router
 
 
 @asynccontextmanager
@@ -22,3 +23,5 @@ app = FastAPI(
 api_version_prefix = "/api/v1"
 
 app.include_router(movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
+
+add_pagination(app)
---
+++ src/pagination/__init__.py
@@ -0,0 +1,1 @@
+from src.pagination.pages import Page, MoviesPage
